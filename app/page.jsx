588e242dc27c2d9f6d017e27fb45a500c20cf392
Wilfried Davo <davowilfried58@gmail.com>
2024-10-16 12:11:10 +0100

    deployement

--- app/page.jsx
+++ app/page.jsx
@@ -16,7 +16,7 @@ const Home = () => {
                         <span className="text-xl">Software Developer</span>
                         <h1 className="h1 mb-6">
                             Hello I'm <br />{" "}
-                            <span className="text-accent">John Doe</span>
+                            <span className="text-accent">Wilfried DAVO</span>
                         </h1>
                         <p className="max-w-[500px] mb-9 text-white/80">
                             Lorem ipsum dolor sit amet consectetur adipisicing
